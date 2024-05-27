7c32c2fb2de19d2bd8224145bbbe425bfaba5d70
ณัชชา บุญยะโท <pulso@sMacBookAir-2.local>
2024-05-27 18:00:31 +0700

    Read me

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
-# React + Vite
+This project was Vite+ React.
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Available Scripts
+In the project directory, you can run:
 
-Currently, two official plugins are available:
+npm install
+First of all, You should run 'npm install' 
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+npm run dev
+Runs the app in the development mode.
+(You will also see any lint errors in the console.)
+
+npm test
+Launches the test runner in the interactive watch mode.
+See the section about running tests for more information
+
+See the section about deployment for more information.
+
--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -7,28 +7,8 @@ function Banner(){
           <h1 className='text-white text-3xl font-light'>STARBUCKS RESERVE</h1>
           <p className='text-lg text-white font-body sm:w-4/5 '>Since 1971, it always has been and will always be about quality. 
           We’re passionate about ethically sourcing only the finest Arabica coffee beans and roasting them with great care. Our passion for
-          coffee is rivaled only by our love of sharing it.'</p>
+          coffee is rivaled only by our love of sharing it.</p>
         </div>
-      
-
-
-
-
-      // <div className="bg-HeroBanner h-96 w-full h-full bg-cover bg-center object-cover"></div>
-      //   <div className="p-96 bg-HeroBanner bg-no-repeat  " >
-      
-      //     <div className="flex flex-col gap-[44px] p-5 py-6 min-h-screen h-auto justify-center md:p-0 lg:px-10 lg:pt-10 lg:pb-20 lg:min-h-fit lg:item-center lg:flex-row lg:justify-between lg:gap-5 overflow-hidden">
-      //     <section className="flex flex-col gap-5 sm:item-center text-center lg:text-left lg:gap-6 lg:item-start">
-      //       <h1 className=" text-white text-[42px] leading -[52px] sm:w-4/5 md:text-5xl md:w-4/5 font-head lg:text-6xl   ">STARBUCKS RESERVE</h1>
-      //       <p className="text-lg text-white font-body sm:w-4/5 ">Since 1971, it always has been and will always be about quality. We’re passionate about ethically sourcing only the finest Arabica coffee beans and roasting them with great care. Our passion for coffee is rivaled only by our love of sharing it.</p>
-      //       <button className="btn btn-primary">Get Started</button>
-      //       </section>
-      //       </div>
-      //     </div>
-     
-      
-     
-      
     )
 }
 export default Banner;
